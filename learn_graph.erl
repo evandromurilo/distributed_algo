a956f23adc4f1a364072d029e2918d4574bbc253
murilo <evandromurilob@hotmail.com>
2025-07-16 00:30:41 -0400

    new state nonstarted_node

--- learn_graph.erl
+++ learn_graph.erl
@@ -21,14 +21,25 @@ unitialized_node() ->
     receive
 	{neighbours, Neighbours} ->
 	    KChan = chan_set(self(), Neighbours),
-	    initialized_node(false, Neighbours, [self()], KChan)
+	    nonstarted_node(Neighbours, [self()], KChan)
     end.
 
+nonstarted_node(Neighbours, KProc, KChan) ->
+    receive
+	start ->
+	    make_first_contact(Neighbours);
+	Pos ->
+	    make_first_contact(Neighbours),
+	    self() ! Pos
+    end,
+    loop(Neighbours, KProc, KChan).
+
+	
 make_first_contact(Neighbours) ->
     io:format("First contact of ~p with ~p!~n", [self(), Neighbours]),
     map(fun(Who) -> Who ! {self(), position, Neighbours} end, Neighbours).
     
-initialized_node(Started, Neighbours, KProc, KChan) ->
+loop(Neighbours, KProc, KChan) ->
     io:format("~p is knowing ~p Kproc and ~p Kchan~n", [self(), KProc, KChan]),
     Done = all(fun({Na, Nb}) -> member(Na, KProc) and member(Nb, KProc) end, KChan),
     if Done ->
@@ -39,29 +50,17 @@ initialized_node(Started, Neighbours, KProc, KChan) ->
     end,
 
     receive
-	start ->
-	    if not Started ->
-		    make_first_contact(Neighbours);
-	       true ->
-		    ok % keep going
-	    end, % se já tinha mandado a primeira comunicação, apenas consumimos a msg start
-	    initialized_node(true, Neighbours, KProc, KChan);
 	{Who, position, HisNeighbours} ->
 	    io:format("~p received pos from ~p!~n", [self(), Who]),
-	    if not Started ->
-		    make_first_contact(Neighbours);
-	       true -> 
-		    ok % keep going
-	    end,
-
+	    
 	    Member = member(Who, KProc),
 	    if not Member ->
 		    map(fun % todo itself
 			   (Other) -> Other ! {Who, position, HisNeighbours} end, Neighbours), % repassa o conhecimento aos vizinhos
-		    initialized_node(true, Neighbours, [Who|KProc], ordsets:union(KChan, chan_set(Who, HisNeighbours)));
+		    loop(Neighbours, [Who|KProc], ordsets:union(KChan, chan_set(Who, HisNeighbours)));
 	       true -> ok % keep going
 	    end,
-	    initialized_node(true, Neighbours, KProc, KChan)
+	    loop(Neighbours, KProc, KChan)
     end.
 
 finished_node() ->
